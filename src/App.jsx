ffb7f526ecea4cf7db6b438c02529791379eb4c5
ibidamola <aisha.adediran0@gmail.com>
2025-03-27 17:36:11 -0400

    added the use of environment variables to main.yml file

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ function App() {
         <h1>Welcome to Vite React Starter!</h1>
         <p>You clicked {count} times</p>
         <button onClick={() => setCount(count + 1)}>Click me</button>
+
+        <h1>Welcome to {import.meta.env.VITE_APP_NAME}!</h1>
+        <p>API URL: {import.meta.env.VITE_API_URL}</p>
       </div>
      
     </>
